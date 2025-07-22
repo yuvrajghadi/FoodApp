61c0f618c7ae716dad21e3939bb7b0bf251a9898
yuvrajghadi <114417851+yuvrajghadi@users.noreply.github.com>
2025-07-23 00:02:18 +0530

    use the swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,39 @@
 import Restocards from "./Restocards";
-import { ResList1 } from "../utils/MockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Body = () => {
 
-const [ResList, setResList] = useState(ResList1)
+const [ResList, setResList] = useState([])
+
+useEffect(() => {
+  fetchData()
+}, []);
+
+
+const fetchData = async () => {
+  try {
+    const data = await fetch(
+      "https://api.allorigins.win/raw?url=" +
+        encodeURIComponent(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0330&lng=73.0297&page_type=DESKTOP_WEB_LISTING"
+        )
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    const restaurantList =
+      json?.data?.cards?.find(
+        (card) =>
+          card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      )?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+    setResList(restaurantList || []);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 
   return (
@@ -19,14 +47,15 @@ const [ResList, setResList] = useState(ResList1)
         <button className="filter-btn" onClick={() => {
             const filtered = ResList.filter(item => item.rating >= 4.5);
             setResList(filtered);
-        }}>Top Rated</button>
+        }}>Top Rated Restaurants</button>
       </div>
 
       <div className="main-content">
         <div className="res-container">
-          {ResList.map((Data) => (
-            <Restocards key={Data.id} resData={Data} />
-          ))}
+         {ResList.map((Data) => (
+  <Restocards key={Data.info.id} resData={Data.info} />
+))}
+
         </div>
       </div>
     </div>
--- src/components/Restocards.js
+++ src/components/Restocards.js
@@ -1,18 +1,23 @@
-
-
-
-
-const Restocards=(props)=>{
-  const { resData } = props;
-  const { image, name, description, rating, price, category } = resData;
+const Restocards = ({ resData }) => {
   return (
-    <div className="res-cards">
-      <img src={image} alt={name} />
-    <h2>{name}</h2>
-    <p>{description}</p>
-    <p>⭐ {rating} | ₹{price}</p>
-    <span>{category}</span>
+    <div className="res-card">
+      <img 
+        className="res-logo"
+        src={
+          resData.cloudinaryImageId
+            ? "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+              resData.cloudinaryImageId
+            : "https://via.placeholder.com/150"
+        }
+        alt={resData.name}
+      />
+      <h3>{resData.name}</h3>
+      <p>⭐ {resData.avgRating} • {resData?.sla?.slaString}</p>
+      <p>{resData.cuisines?.join(", ")}</p>
+      <p>{resData.areaName}</p>
+      <p>{resData.costForTwo}</p>
     </div>
   );
-}
+};
+
 export default Restocards;
