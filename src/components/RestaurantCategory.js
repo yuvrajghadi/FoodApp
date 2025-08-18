b096cca99d088d56e4f40aa5f91083cadba252c3
yuvrajghadi <114417851+yuvrajghadi@users.noreply.github.com>
2025-08-18 14:54:36 +0530

    improve accordion

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,22 +1,21 @@
 import ItemList from "./ItemList";
-import { useState } from "react";
+
 import './index.css';
 
 
-const RestaurantCategory=({data})=>{
+const RestaurantCategory=({data , showItems , setShowIndex})=>{
 
-    // console.log("RestrauntCategory data",data)
-    const [showItems, setShowItems] = useState(true);
+
 
 const handleClick=()=>{
-    setShowItems(!showItems);
+    setShowIndex()
 }
 
     return(
         <div className="items">
             <div >
                 <div className="recommendation" onClick={handleClick}>
-                      <span className="font-bold py-2 px-4 bg-white shadow-md rounded-lg hover:shadow-lg transition-shadow ">{data?.title || "Recommendation"} ({data?.itemCards?.length || 0}) </span>
+                      <span className="py-2 px-4 bg-white shadow-md rounded-lg hover:shadow-lg transition-shadow ">{data?.title || "Recommendation"} ({data?.itemCards?.length || 0}) </span>
             <span className="category-description">{showItems ? "🔻" : "🔺"}</span>
                 </div>
               
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,17 +1,17 @@
-
 import Shimmer from "./Shimmer";
 import "./RestaurantMenu.css";
 import Deals from "./Deals";
 import Manusearch from "./Manusearch";
 import TopPicks from "./TopPicks";
-import { useParams } from "react-router-dom"; 
+import { useParams } from "react-router-dom";
 import useRestraurantMenu from "../utils/useRestaurantManu";
 import RestaurantCategory from "./RestaurantCategory";
-
+import { useState } from "react";
 
 const RestaurantMenu = () => {
-   const {resId} = useParams();
-   const  menuData  = useRestraurantMenu(resId);
+  const { resId } = useParams();
+  const menuData = useRestraurantMenu(resId);
+  const [showIndex, setShowIndex] = useState(0);
 
   if (menuData === null) {
     return <Shimmer />;
@@ -25,42 +25,52 @@ const RestaurantMenu = () => {
     totalRatingsString,
     areaName,
   } = menuData?.cards?.[2]?.card?.card?.info || {};
-//   const {slaString} =
-//     menuData?.cards?.[2]?.card?.card?.info?.sla?.slaString || "";
-//     console.log(menuData?.cards?.[2]?.card?.card?.info)
-// console.log("SLA String:", slaString);
-    const itemCards =
-  menuData?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[2]?.card?.card?.itemCards || [];
+  //   const {slaString} =
+  //     menuData?.cards?.[2]?.card?.card?.info?.sla?.slaString || "";
+  //     console.log(menuData?.cards?.[2]?.card?.card?.info)
+  // console.log("SLA String:", slaString);
+  const itemCards =
+    menuData?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[2]?.card
+      ?.card?.itemCards || [];
 
-  const categories = menuData?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(c=>c?.card?.card?.["@type"]==
-    "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-  ) || [];
+  const categories =
+    menuData?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+      (c) =>
+        c?.card?.card?.["@type"] ==
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    ) || [];
   console.log("categories:", categories);
 
   return (
     <div className="menu-container">
       <div className="menu-card ">
         <h1 className="menu-title">{name}</h1>
-<div className="hotel-details">
-        <p className="menu-rating">
-          ⭐{avgRatingString} ({totalRatingsString}) .₹{((costForTwo ?? 0) / 100).toFixed(2)}
+        <div className="hotel-details">
+          <p className="menu-rating">
+            ⭐{avgRatingString} ({totalRatingsString}) .₹
+            {((costForTwo ?? 0) / 100).toFixed(2)}
+          </p>
+          <p>{cuisines?.join(", ") || "N/A"}</p>
 
-        </p>
-      <p>{cuisines?.join(", ") || "N/A"}</p>
-
-        <p>{areaName}</p>
-      
+          <p>{areaName}</p>
         </div>
         <Deals />
         <Manusearch />
         <TopPicks />
         <div className="bar"></div>
         {categories.map((category, index) => (
-          <RestaurantCategory className="res-card-link" key={index} data={category?.card?.card} />
+          <RestaurantCategory
+            className="res-card-link"
+            key={index}
+            data={category?.card?.card}
+            showItems={index === showIndex ? true : false}
+            setShowIndex={() =>
+              setShowIndex(index === showIndex ? null : index)
+            }
+          />
         ))}
       </div>
     </div>
-
   );
 };
 
